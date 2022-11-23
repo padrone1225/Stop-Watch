55e480c14f146893bc86f6511d106e6154a8c066
padrone1225 <lidinfomaborisovich@gmail.com>
2022-11-24 04:17:46 +0900

    rdesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { clearInterval } from "timers";
 import "./App.css";
 
 function App() {
-  const [title, setTitle] = useState(false);
+  const [title, setTitle] = useState(true);
   const [timer, setTimer] = useState(300);
   const [watch, setWatch] = useState(0);
   const [timerState, setTimerState] = useState(false);
@@ -17,14 +18,14 @@ function App() {
 
   useEffect(() => {
     if (title && timerState) {
-      let T = setInterval(() => {
+      let TT = setInterval(() => {
         setTimer((prev) => {
           if (prev !== 0) return prev - 1;
           return 300;
         });
       }, 1000);
 
-      return () => clearInterval(T);
+      return () => clearInterval(TT);
     }
     if (!title && timerState) {
       let T = setInterval(() => {
@@ -35,50 +36,53 @@ function App() {
 
       return () => clearInterval(T);
     }
+    // return () => {
+    //   clearInterval(TT);
+    // };
   }, [timerState, title]);
 
   return (
     <div className="bg-slate-300 relative h-screen">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[200px] w-[400px] border-slate-700 border-2">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[200px] w-[400px] border-slate-700 border-2 rounded-xl">
         <div className="flex" id="header">
           <div
-            className="cursor-pointer px-10 py-5 hover:bg-slate-400 w-1/2 text-center"
+            className="cursor-pointer px-10 py-5 hover:bg-slate-400 w-1/2 text-center rounded-[12px_0_0_0]"
             onClick={() => setTitle(true)}
           >
             TIMER
           </div>
           <div
-            className="cursor-pointer px-10 py-5 hover:bg-slate-400 w-1/2 text-center"
+            className="cursor-pointer px-10 py-5 hover:bg-slate-400 w-1/2 text-center rounded-[0_12px_0_0]"
             onClick={() => setTitle(false)}
           >
             STOPWATCH
           </div>
         </div>
         {title ? (
-          <div className="flex gap-2 my-5 mx-auto pl-5">
-            <div id="min">{Math.floor(timer / 60)} m</div>
+          <div className="flex gap-2 my-5 mx-auto pl-5 items-end justify-center">
+            <div id="min" className="text-xl">
+              {Math.floor(timer / 60)} m
+            </div>
             <div id="s">{timer % 60} s</div>
           </div>
         ) : (
-          <div className="flex gap-2 my-5 mx-auto pl-5">
+          <div className="flex gap-2 my-5 mx-auto pl-5 items-end justify-center">
             <div id="sec">{Math.floor(watch / 100)} s</div>
             <div id="msec">{watch % 100}</div>
           </div>
         )}
-        <div className="flex" id="footer">
-          <div className="grid grid-cols-3 gap-3">
-            <div
-              className="cursor-pointer bg-white text-blue-700 p-3 border-black border-2"
-              onClick={() => setTimerState(!timerState)}
-            >
-              {timerState ? "STOP" : "START"}
-            </div>
-            <div
-              className="cursor-pointer bg-blue-700 text-white p-3 border-cyan-500 border-2"
-              onClick={setClean}
-            >
-              RESET
-            </div>
+        <div className="flex justify-around" id="footer">
+          <div
+            className="cursor-pointer bg-white text-blue-700 p-3 border-black border-2"
+            onClick={() => setTimerState(!timerState)}
+          >
+            {timerState ? "STOP" : "START"}
+          </div>
+          <div
+            className="cursor-pointer bg-blue-700 text-white p-3 border-cyan-500 border-2"
+            onClick={setClean}
+          >
+            RESET
           </div>
         </div>
       </div>
