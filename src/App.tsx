8c87a64bda21afd17edaad59b45b770dcd876b72
padrone1225 <lidinfomaborisovich@gmail.com>
2022-11-24 18:05:03 +0900

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { clearInterval } from "timers";
 import "./App.css";
 
 function App() {
@@ -7,8 +6,13 @@ function App() {
   const [timer, setTimer] = useState(300);
   const [watch, setWatch] = useState(0);
   const [timerState, setTimerState] = useState(false);
+  const [watchState, setWatchState] = useState(false);
+  const [timerIntervalId, setTimerIntervalId] = useState(0);
+  const [watchIntervalId, setWatchIntervalId] = useState(0);
 
   const setClean = () => {
+    if (timerIntervalId !== 0) clearInterval(timerIntervalId);
+    if (watchIntervalId !== 0) clearInterval(watchIntervalId);
     if (title) {
       setTimer(300);
     } else {
@@ -16,47 +20,72 @@ function App() {
     }
   };
 
+  const handleClick = () => {
+    if (title) {
+      setTimerState(!timerState);
+    } else {
+      setWatchState(!watchState);
+    }
+  };
+
   useEffect(() => {
     if (title && timerState) {
-      let TT = setInterval(() => {
+      let T: any = setInterval(() => {
         setTimer((prev) => {
           if (prev !== 0) return prev - 1;
           return 300;
         });
       }, 1000);
+      setTimerIntervalId(T);
 
-      return () => clearInterval(TT);
+      return () => clearInterval(T);
+    } else if (title && !timerState) {
+      clearInterval(timerIntervalId);
     }
-    if (!title && timerState) {
-      let T = setInterval(() => {
+
+    if (!title && watchState) {
+      let T: any = setInterval(() => {
         setWatch((prev) => {
           return prev + 1;
         });
       }, 10);
+      setWatchIntervalId(T);
 
       return () => clearInterval(T);
+    } else if (!title && !watchState) {
+      clearInterval(watchIntervalId);
     }
-    // return () => {
-    //   clearInterval(TT);
-    // };
-  }, [timerState, title]);
+  }, [timerState, title, watchState]);
 
   return (
     <div className="bg-slate-300 relative h-screen">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[200px] w-[400px] border-slate-700 border-2 rounded-xl">
         <div className="flex" id="header">
-          <div
-            className="cursor-pointer px-10 py-5 hover:bg-slate-400 w-1/2 text-center rounded-[12px_0_0_0]"
-            onClick={() => setTitle(true)}
-          >
-            TIMER
-          </div>
-          <div
-            className="cursor-pointer px-10 py-5 hover:bg-slate-400 w-1/2 text-center rounded-[0_12px_0_0]"
-            onClick={() => setTitle(false)}
-          >
-            STOPWATCH
-          </div>
+          {title ? (
+            <>
+              <div className="cursor-pointer w-1/2 px-10 py-5 uppercase text-center rounded-[12px_0_0_0] border-b-2 border-green-600">
+                timer
+              </div>
+              <div
+                className="cursor-pointer px-10 py-5 hover:bg-slate-400 w-1/2 text-center rounded-[0_12px_0_0]"
+                onClick={() => setTitle(false)}
+              >
+                STOPWATCH
+              </div>
+            </>
+          ) : (
+            <>
+              <div
+                className="cursor-pointer px-10 py-5 hover:bg-slate-400 w-1/2 text-center rounded-[12px_0_0_0]"
+                onClick={() => setTitle(true)}
+              >
+                TIMER
+              </div>
+              <div className="cursor-pointer w-1/2 px-10 py-5 uppercase text-center rounded-[0_12px_0_0] border-b-2 border-green-600">
+                stopwatch
+              </div>
+            </>
+          )}
         </div>
         {title ? (
           <div className="flex gap-2 my-5 mx-auto pl-5 items-end justify-center">
@@ -74,7 +103,7 @@ function App() {
         <div className="flex justify-around" id="footer">
           <div
             className="cursor-pointer bg-white text-blue-700 p-3 border-black border-2"
-            onClick={() => setTimerState(!timerState)}
+            onClick={() => handleClick()}
           >
             {timerState ? "STOP" : "START"}
           </div>
